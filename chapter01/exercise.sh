759ca5ce4a1700c01d8dff1fa32c3f3a37abe5a2
zhaohuizh <bravezhaohui@gmail.com>
2014-05-10 00:33:48 -0400

    exercise

--- chapter01/exercise.sh
+++ chapter01/exercise.sh
@@ -2,3 +2,17 @@
 
 clear
 
+echo "bash program is located at /bin/bash"
+echo
+
+bash --version
+echo
+
+echo "/etc/profile, ~/.bash_profile, ~/.bash_login or ~/.profile"
+echo
+
+COUNT=`ps -l | wc -l`
+echo "Total processes are running: `expr $COUNT - 1`"
+echo
+
+echo `hostname`
